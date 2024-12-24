2b5f3440eae93feef6c38edd14e1e647a07f3e8e
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-12-24 18:40:43 +0100

    Apply the ETL

--- DataWarehouse/Scripts/LoadDimensions.sql
+++ DataWarehouse/Scripts/LoadDimensions.sql
@@ -1,5 +1,21 @@
 INSERT INTO Calendrier (ClefDate, Date, Jour, Semaine, Mois, Trimestre, Annee, Ferie)
-SELECT ClefDate, Date, Jour, Semaine, Mois, Trimestre, Annee, Ferie
+SELECT ClefDate, Date, 
+       CASE 
+           WHEN Jour = 'Monday' THEN 1
+           WHEN Jour = 'Tuesday' THEN 2
+           WHEN Jour = 'Wednesday' THEN 3
+           WHEN Jour = 'Thursday' THEN 4
+           WHEN Jour = 'Friday' THEN 5
+           WHEN Jour = 'Saturday' THEN 6
+           WHEN Jour = 'Sunday' THEN 7
+           ELSE NULL
+       END AS Jour,
+       Semaine, Mois, Trimestre, Annee, 
+       CASE 
+           WHEN Ferie = 'Oui' THEN 1
+           WHEN Ferie = 'Non' THEN 0
+           ELSE NULL
+       END AS Ferie
 FROM StagingCalendrier;
 
 INSERT INTO Temps (ClefTemps, Temps, Heure, Minute, AM_PM, JourNuit, HeureCreuse)
