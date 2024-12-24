77b8e8e74c575f5e2cc3f252b1591af32787b054
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-12-24 11:20:31 +0100

    Delete the Staging tables

---
+++ DataWarehouse/Scripts/DeleteStagingTabs.sql
@@ -0,0 +1,10 @@
+-- Drop Staging Tables
+DROP TABLE IF EXISTS Staging_Calendrier;
+DROP TABLE IF EXISTS Staging_Temps;
+DROP TABLE IF EXISTS Staging_Caisse;
+DROP TABLE IF EXISTS Staging_Magasin;
+DROP TABLE IF EXISTS Staging_Promotion;
+DROP TABLE IF EXISTS Staging_Produit;
+DROP TABLE IF EXISTS Staging_Client;
+DROP TABLE IF EXISTS Staging_ModePaiement;
+DROP TABLE IF EXISTS Staging_VenteProduits;
